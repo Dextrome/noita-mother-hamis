de12ffa6b8ea832c4410e59f22665cdd5f40aeda
Dextrome <Kennyd.Sevenants@gmail.com>
2022-04-16 23:57:18 +0200

    maxuses etc

--- files/scripts/gun_actions.lua
+++ files/scripts/gun_actions.lua
@@ -7,10 +7,11 @@ table.insert( actions,
     sprite_unidentified = "data/ui_gfx/gun_actions/spiral_shot_unidentified.png",
     related_projectiles	= {"mods/dextrome_mother_hamis/files/projectiles/summon_mother_hamis.xml"},
     type 		= ACTION_TYPE_PROJECTILE,
-    spawn_level                       = "1,2,3,4,5,6", -- SPIRAL_SHOT
-    spawn_probability                 = "0.2,0.2,0.5,0.5,0.9,0.9", -- SPIRAL_SHOT
-    price = 500,
+    spawn_level                       = "1,2,3,4,5,6", 
+    spawn_probability                 = "0.2,0.2,0.5,0.5,0.9,0.9", 
+    price = 450,
     mana = 200,
+	max_uses    = 3, 
     action 		= function()
         add_projectile("mods/dextrome_mother_hamis/files/projectiles/summon_mother_hamis.xml")
     end,
